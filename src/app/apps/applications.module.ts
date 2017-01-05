7793f6497d9cc9061a5e9b6e012aab7ad304369e
Alexei Eraskin <levrun1@gmail.com>
2017-01-05 03:47:11 +0100

    first version of carousel with materialisse

--- src/app/apps/applications.module.ts
+++ src/app/apps/applications.module.ts
@@ -14,6 +14,7 @@ import { AppListComponent  } from './app-list/app-list.component';
 import { applicationsRouting } from './application.routing';
 import { TopAppsComponent } from './top-apps/top-apps.component';
 import { SharedModule } from '../shared/shared.module';
+import { MaterializeModule } from "angular2-materialize";
 
 @NgModule({
   declarations: [
@@ -29,7 +30,10 @@ import { SharedModule } from '../shared/shared.module';
     AppsStartComponent,
     TopAppsComponent
   ],
-  imports: [SharedModule, ReactiveFormsModule, applicationsRouting]
+  imports: [SharedModule,
+            ReactiveFormsModule,
+            applicationsRouting,
+            MaterializeModule]
 
 })
 export class ApplicationsModule {}
--- src/app/apps/top-apps/top-apps.component.ts
+++ src/app/apps/top-apps/top-apps.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { Application } from '../../shared/application';
+import { ApplicationCategory } from "../models/application-category";
+import { ApplicationService } from '../services/application.service';
 
 @Component({
   selector: 'as-top-apps',
@@ -8,6 +10,24 @@ import { Application } from '../../shared/application';
 })
 export class TopAppsComponent {
 
+  applications: Application[] = [];
+  selectedApplicationCategory: ApplicationCategory;
+  isDataAvailable = false;
 
+  constructor(private applicationService: ApplicationService) {}
+
+  ngOnInit() {
+    console.log("ngOnInit()");
+    console.log("isDataAvailable " + this.isDataAvailable);
+    this.applicationService.appsCategoriesChanged.subscribe(
+      (applicationsCategories: ApplicationCategory[]) => {
+          if(this.isDataAvailable === false) {
+            this.applications = applicationsCategories[0].applications
+            this.isDataAvailable = true;
+          }
+        }
+    );
+    this.applicationService.getApplicationsCategories();
+  }
 
 }
