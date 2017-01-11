7de936ca2c12ef68ceb2db2bad5b02b4a4dc48c6
Alexei Eraskin <levrun1@gmail.com>
2017-01-12 00:00:32 +0100

    update home component with helpful images and comments

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -1,10 +1,14 @@
 import { DropdownDirective } from './dropdown.directive';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
     DropdownDirective, HomeComponent
+  ],
+  imports: [
+    routing
   ],
   exports: [DropdownDirective]
 
